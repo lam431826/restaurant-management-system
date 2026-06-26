f15c5bfac5be61716171f4f8624a05c91e1d1d04
maichiph <phanmaichi184@gmail.com>
2026-06-26 11:43:35 +0700

    handle responsive in menu card

--- Frontend/PublicWebsite/src/components/Footer.jsx
+++ Frontend/PublicWebsite/src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
         </span>
         <Diamond />
         <div className="text-[#efe7d2] text-sm leading-[1.5]" style={{ fontFamily: "'MJ Satoshi', sans-serif" }}>
-          <p className="m-0">SĐT: 0975919999</p>
+          <p className="m-0">SĐT: 0975919989</p>
           <p className="m-0">Email: wasabisushi@gmail.com</p>
         </div>
         <Diamond />
